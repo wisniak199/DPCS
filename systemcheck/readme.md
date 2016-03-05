68bbf1f294068423ce2fcbc2d8010b79eaef8406
Pawel Golinski <golpaw1@gmail.com>
2016-03-05 16:32:33 +0100

    Ticket #7 - System check module

---
+++ systemcheck/readme.md
@@ -0,0 +1,2 @@
+Small Python2 script that returns system data (kernel, architecture,
+installed packages) in JSON.
---
+++ systemcheck/systemcheck.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import platform
+import os
+import re
+import json
+
+if __name__ == "__main__":
+    uname = platform.uname()
+    platform_dict = {
+        "system_name": uname[1],
+        "kernel": uname[2],
+        "architecture": uname[4]
+    }
+    package_list = []
+    # get installed packages from dpkg
+    packages_raw_info = \
+        os.popen(
+            "dpkg-query -W -f='${binary:Package}\t${Version}\t${Status}\n' | grep \"install ok installed\""
+        ).read()
+    packages_raw_info = packages_raw_info.split("\n")
+    for line in packages_raw_info:
+        if line:
+            package_re = re.compile(r"(.+)\t(.+)\t.*")
+            package_info = package_re.match(line)
+            package_dict = {
+                "name": package_info.group(1),
+                "version": package_info.group(2)
+            }
+            package_list.append(package_dict)
+    data = {}
+    data["platform"] = platform_dict
+    data["packages"] = package_list
+    json_data = json.dumps(data)
+    print json_data

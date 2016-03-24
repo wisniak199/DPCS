aefbe2b6ca29dc6103788aae59566bba69dd4309
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-03-24 17:29:43 +0100

    Add browser sync

--- client/frontend/gulpfile.js
+++ client/frontend/gulpfile.js
@@ -5,10 +5,16 @@ var sourcemaps = require('gulp-sourcemaps');
 var minifyCSS = require('gulp-minify-css');
 var minify = require('gulp-minify');
 var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+
 
 var paths = {
     less: './less/**/*.less',
-    scripts: './js/**/*.js'
+    htmls: './public_html/*.html',
+    scripts: [
+        './js/libs/jquery.js', //jquery must be befeore bootstrap
+        './js/**/*.js'
+    ]
 };
 
 gulp.task('less', function () {
@@ -18,7 +24,8 @@ gulp.task('less', function () {
         .pipe(minifyCSS())
         .pipe(sourcemaps.write())
         .pipe(concat('app.min.css'))
-        .pipe(gulp.dest('./public_html/css'));
+        .pipe(gulp.dest('./public_html/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function () {
@@ -31,6 +38,15 @@ gulp.task('scripts', function () {
 gulp.task('default', ['less', 'scripts']);
 
 gulp.task('watch', function () {
-    gulp.watch(paths.scripts, ['scripts']);
+
+    browserSync.init({
+        server: {
+            baseDir: "./public_html"
+        },
+        startPath: "/index.html"
+    });
+
+    gulp.watch(paths.scripts, ['scripts'],browserSync.reload);
+    gulp.watch(paths.htmls).on('change', browserSync.reload);
     gulp.watch(paths.less, ['less']);
 });
--- client/frontend/reademe.md
+++ client/frontend/reademe.md
@@ -7,5 +7,9 @@ Lorem IGNACY ipsum:
  1. Install node.js from https://nodejs.org/
  2. Open terminal at `DPCS\client\frontend`
  3. Type command `npm install` to restore packages
- 4. Type command `gulp` to build app
+ 4. Run `default` task in gulp
+    * Type command `gulp` to build app or
+    * Run `default` task from your IDE
  5. Open `public_html\index.html` in web browser
+
+ To enable browser sync run `watch` task. When you edit file then gulp detect it and reload web page in your browser

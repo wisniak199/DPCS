77a9026eea6fd66232aa9810952abafbba8ba8be
Marek Bardoński <bdfhjk@gmail.com>
2016-03-25 12:50:56 +0100

    Remove old server folder

--- alpha_server/server_django/alpha/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CrashGroup, Application, CrashReport, SystemInfo, Solution
-# Register your models here.
-
-admin.site.register(CrashGroup)
-admin.site.register(Application)
-admin.site.register(CrashReport)
-admin.site.register(SystemInfo)
-admin.site.register(Solution)

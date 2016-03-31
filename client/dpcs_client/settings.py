65a805a177452481600279c5b852fa8e43a1d831
mateuszsusik <mateuszsusik@gmail.com>
2016-03-31 13:01:38 +0200

    client: settings file check (closes #65)
    Signed-off-by: mateuszsusik <mateuszsusik@gmail.com>

--- client/dpcs_client/settings.py
+++ client/dpcs_client/settings.py
@@ -6,13 +6,18 @@ import os
 DEFAULT_SERVER_ADDRESS = "http://private-a6e53-dpcs.apiary-mock.com/"
 FILE = os.path.expanduser('~/.dpcs/.dpcsconfig')
 
+DEFAULT_SETTINGS = {'server_address': DEFAULT_SERVER_ADDRESS}
+
 
 def read_settings():
-    with open(FILE, 'r') as f:
-        try:
-            settings = json.load(f)
-            if 'server_address' not in settings:
-                settings['server_address'] = DEFAULT_SERVER_ADDRESS
-        except ValueError:
-            settings = {'server_address': DEFAULT_SERVER_ADDRESS}
-        return settings
+    try:
+        with open(FILE, 'r') as f:
+            try:
+                settings = json.load(f)
+                if 'server_address' not in settings:
+                    settings['server_address'] = DEFAULT_SERVER_ADDRESS
+            except ValueError:
+                settings = DEFAULT_SETTINGS
+            return settings
+    except IOError:
+        return DEFAULT_SETTINGS
